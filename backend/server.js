9d18f8d98aa3d532f48a1ac11730c561fc1b6763
Thomas Crowe <wcrowe@g.clemson.edu>
2022-09-18 23:25:35 -0400

    Cleaned up react extras, created components folder

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 require('dotenv').config();
 var mongoose = require('mongoose');
-var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/yourDB';
+var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project2';
 var port = process.env.PORT || 4000;
 var router = express.Router();
 
 
 //Database
-// mongoose.connect(mongoURI, () => console.log(`MongoDB Connected at ${mongoURI}`));
+mongoose.connect(mongoURI, () => console.log(`MongoDB Connected at ${mongoURI}`));
 
 //Middleware
 app.use(express.json());
